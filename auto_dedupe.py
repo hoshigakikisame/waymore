4b2bbbe7280701f4df00305f6b82361778f3eb20
hoshigakikisame <rianatgithub@gmail.com>
2025-05-31 10:19:39 +0000

    refactor: more convenient file splitting

--- auto_dedupe.py
+++ auto_dedupe.py
@@ -5,7 +5,7 @@ import time
 import re
 import argparse
 
-def auto_dedupe(substr: str, outdir: str):
+def auto_dedupe(substr: str, outdir: str, delresult: bool):
     # Get the current working directory
     cwd = os.getcwd()
     # Get the list of files in the current working directory
@@ -14,10 +14,18 @@ def auto_dedupe(substr: str, outdir: str):
     # custom callback function to sort the files
     files.sort(key=lambda x: int(re.findall(r'\d', x)[0]))
     # Iterate over the list of files
-    if len(files) > 1:
-        for file in files[:len(files)-1]:
-            print(f'Deduping {file}')
-            os.system(f'./dedupe.sh {file} {outdir} > /dev/null 2>&1')
+    if len(files) > 0:
+        print(files)
+        for file in files:
+            print(f'[+] Deduping {file}')
+            cmd = f'cat {file} | urldedupe > "{outdir}/urldedupe.{file}" && rm -rf {file}'
+            os.system(cmd)
+            print(f'[+] Sending Notification')
+            cmd = f'kabarin -f {outdir}/urldedupe.{file} -cs 500'
+            os.system(cmd)
+            if delresult:
+                cmd = f'rm -rf {outdir}/urldedupe.{file}'
+                os.system(cmd)
     else:
         print('No files to dedupe')
 
@@ -25,8 +33,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Auto-dedupe wayback.urls files')
     parser.add_argument('--substr', type=str, help='Substring to match in the file name')
     parser.add_argument('--outdir', type=str, help='Output directory to store the deduped files')
+    parser.add_argument('--sleepdur', type=int, help='Minutes to wait for the next execution', default=5)
+    parser.add_argument('--delresult', action=argparse.BooleanOptionalAction, help='Delete result upon storing file in telegram', default=True)
     args = parser.parse_args()
 
     while True:
-        auto_dedupe(args.substr, args.outdir)
-        time.sleep(60*5) # Sleep for 5 minutes
+        auto_dedupe(args.substr, args.outdir, args.delresult)
+        print(f'[+] Sleeping for {args.sleepdur} min')
+        time.sleep(60*args.sleepdur)
